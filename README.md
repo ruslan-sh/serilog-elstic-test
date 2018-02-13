d3f2a91a567a6dcddcec560c08d9c74683d92ac0
ruslan <ruslan-sh@taxnet.ru>
2018-02-13 18:29:58 +0300

    Log writes to elastic

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # serilog-elstic-test
 Test of Serilog + Elastic Search using
+
+## Configuration ##
+
+1. Install ElasticSearch + Kibana via docker:    
+    ```sh
+    docker run -d -p 9200:9200 -p 5601:5601 nshou/elasticsearch-kibana
+    ```
---
+++ src/Console/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.File;
+using System;
+
+namespace RuslanSh.SerilogElasticTest.ConsoleClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var consoleLogger = new LoggerConfiguration()
+				.WriteTo.Console()
+				.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+				{
+					AutoRegisterTemplate = true,
+					FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+					EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+									   EmitEventFailureHandling.WriteToFailureSink |
+									   EmitEventFailureHandling.RaiseCallback,
+					FailureSink = new FileSink("./failures.txt", new JsonFormatter(), null)
+				})
+				.CreateLogger();
+		}
+	}
+}
